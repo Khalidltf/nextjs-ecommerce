798a88100519e8c83ffdb5a92ffb0ab9f24bbf9b
khalidlotfi <khalidlotfi2005@gmail.com>
2024-05-12 07:49:05 +0100

    Product page details, generate Meta dATA + REaCT Cache

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function NotFoundPage() {
+  return (
+    <>
+      <h1>Not FounDD)</h1>
+      <Link href={"/"} className="btn-link btn">
+        Back Home
+      </Link>
+    </>
+  );
+}
